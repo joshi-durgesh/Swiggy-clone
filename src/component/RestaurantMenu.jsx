74ec8adfe90d4a034f1220406f25ce7dd2c758e8
joshi-durgesh <durgesh7joshi9@gmail.com>
2024-02-09 23:03:16 +0530

    Revert "menu item added"
    This reverts commit 15d18e818cd800cc6f92fe84236c420d1501ebb9.

--- src/component/RestaurantMenu.jsx
+++ src/component/RestaurantMenu.jsx
@@ -1,8 +1,6 @@
 import useRestaurantMenu from "../utils/useRestaurantMenu";
 import { BannerSkimmers } from "./Skimmer";
 import { useParams } from "react-router-dom";
-import RestaurantDetails from "./RestaurantDetails";
-import RestaurantCuisineList from "./RestaurantCuisineList";
 
 const RestaurantMenu = () => {
   const { resId } = useParams();
@@ -11,10 +9,30 @@ const RestaurantMenu = () => {
 
   if (menuData === null) return <BannerSkimmers />;
 
+  const { name, cuisines, areaName, sla, avgRating, totalRatingsString } =
+    menuData?.data?.cards[0]?.card?.card?.info;
+
+  const { lastMileTravelString } = sla;
+
   return (
     <section className='max-w-3xl mx-auto '>
-      <RestaurantDetails menuData={menuData} />
-      <RestaurantCuisineList menuData={menuData} />
+      <div className='flex justify-between pb-5 border-b-[1px] border-b-slate-400 border-dashed mt-9'>
+        <div>
+          <h2 className='font-bold text-lg mb-2'>{name}</h2>
+          <p className='text-[13px] text-slate-500'>{cuisines.join(",")}</p>
+          <p className='text-[13px] text-slate-500'>
+            {areaName},{lastMileTravelString}
+          </p>
+        </div>
+        <div className='flex flex-col justify-center border-[1px] border-slate-300 items-center rounded-xl p-3'>
+          <span className='bi bi-star-fill pb-1 text-green-700 font-bold'>
+            &nbsp;{avgRating}
+          </span>
+          <span className='pt-1 text-slate-400 border-t-[1px] font-semibold'>
+            {totalRatingsString}
+          </span>
+        </div>
+      </div>
     </section>
   );
 };
