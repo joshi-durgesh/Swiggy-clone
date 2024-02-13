e51879f962b65df8e1a813db946fb7eb0ba18412
joshi-durgesh <durgesh7joshi9@gmail.com>
2024-02-13 15:59:52 +0530

    contact us page  created

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -1,5 +1,24 @@
 const Contact = () => {
-  return <div>Contact Section</div>;
+  return (
+    <div>
+      <h1 className='font-bold text-3xl p-4 m-4'>Contact Us Page</h1>
+      <form>
+        <input
+          type='text'
+          placeholder='Name'
+          className='border border-black p-2 m-2'
+        />
+        <input
+          type='text'
+          placeholder='message'
+          className='border border-black p-2 m-2'
+        />
+        <button className='border border-black p-2 m-2 bg-green-200 rounded-xl'>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Contact;
